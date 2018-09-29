9a6fad8cbd08d1a91522fa15698e76cc7322f5e9
lennonpetrick <lennonpetrick@gmail.com>
2018-09-29 13:51:35 -0300

    first commit

---
+++ app/src/main/java/com/test/myweather/WeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.test.myweather
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+
+}
